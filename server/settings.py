af8aaa78f393328c423a9a9b9c4cee1a575b9ef2
Ivan Jelic <ivan.jelic@sourcefabric.org>
2026-07-14 13:02:36 +0200

    Removing Planning and Analytics from server settings

--- server/settings.py
+++ server/settings.py
@@ -18,12 +18,7 @@ init_data = Path(ABS_PATH) / "data"
 if init_data.exists():
     INIT_DATA_PATH = init_data
 
-INSTALLED_APPS = [
-    "planning",
-    "analytics",
-]
-
-MODULES.append("planning")
+INSTALLED_APPS = []
 
 PLANNING_EVENT_TEMPLATES_ENABLED = True
 
